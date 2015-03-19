01a78df5c0848f2191ed6ac9643d8f22a42f2ef4
colml <colm77@gmail.com>
2015-03-19 16:04:21 +0800

    Initial commit

---
+++ app/code/local/CP/PaymentTerms/Helper/Data.php
@@ -0,0 +1,5 @@
+<?php
+class CP_PaymentTerms_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+} 
---
+++ app/code/local/CP/PaymentTerms/Model/Onetwentydays.php
@@ -0,0 +1,12 @@
+<?php
+    class CP_PaymentTerms_Model_Onetwentydays extends Mage_Payment_Model_Method_Abstract
+    {
+        /**
+        * unique internal payment method identifier
+        *
+        * @var string [a-z0-9_]
+        */
+        protected $_code = 'onetwentydays';
+     
+    }
+?>
